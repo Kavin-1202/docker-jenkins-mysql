23ece1de939c97227834156f419a1e09252ff9d9
KavinG <kavinvelavan122@gmail.com>
2024-07-29 15:22:34 +0530

    update

--- src/test/java/com/ust/docker_jenkin_springboot/DockerJenkinSpringbootApplicationTests.java
+++ src/test/java/com/ust/docker_jenkin_springboot/DockerJenkinSpringbootApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DockerJenkinSpringbootApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
